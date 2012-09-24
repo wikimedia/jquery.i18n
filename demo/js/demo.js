efbfa9f53628bb44b003d777f7002527f34a41ad
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2012-09-23 17:42:11 -0700

    More translations for demo

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -3,17 +3,18 @@
 
 	$( document ).ready(
 			function () {
+				var i18n = $.i18n();
 				var message = "$1 has $2 {{plural:$2|kitten|kittens}}. "
 						+ "{{gender:$3|He|She}} loves to play with {{plural:$2|it|them}}.";
 				var updateText = function () {
 					var language = $( '.language option:selected' ).val();
-					$.i18n( {
-						locale: language
-					} );
+					i18n.locale = language;
 					var person = $.i18n( $( '.person option:selected' ).text() );
 					var sex = $( '.person option:selected' ).val();
 					var kittens = $( '.kittens' ).val();
-					$( '.result' ).text( $.i18n( message, person, kittens, sex ) );
+					$( '.result' )
+						.text( $.i18n( message, person, kittens, sex ) )
+						.prop( 'title', message.toLocaleString() );
 				};
 				updateText();
 				$( '.kittens, .person, .language' ).on( 'change', updateText );
