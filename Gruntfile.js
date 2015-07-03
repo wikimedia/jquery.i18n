6ae9cf80c66c521b43dac2f18dd87a79fdffda9f
James D. Forrester <jforrester@wikimedia.org>
2015-07-03 14:37:23 -0700

    build: Actually provide `grunt watch`

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,8 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
 	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
 		connect: {
@@ -23,6 +25,14 @@ module.exports = function ( grunt ) {
 			options: JSON.parse( grunt.file.read( '.jshintrc' )
 				.replace( /\/\*(?:(?!\*\/)[\s\S])*\*\//g, '' ).replace( /\/\/[^\n\r]*/g, '' ) ),
 			all: [ 'src/**/*.js' ]
+		},
+		watch: {
+			files: [
+				'.{jshintignore,jshintrc}',
+				'Gruntfile.js',
+				'<%= jshint.all %>'
+			],
+			tasks: 'test'
 		},
 		qunit: {
 			all: {
